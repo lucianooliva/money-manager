e88744fe3a824fdc0e072eff1d6bcec66e32f43f
Luciano Oliva <luciano@seg.eti.br>
2024-04-04 20:16:03 -0300

    Add router

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,16 +4,46 @@ import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
 import DefaultLayout from '../views/DefaultLayout.vue'
+import MonthlyBills from "../views/MonthlyBills.vue"
+import MonthlyIncomes from "../views/MonthlyIncomes.vue"
+import Category from "../views/Category.vue"
+import ExpenseEditor from "../views/ExpenseEditor.vue"
+import IncomeEditor from "../views/IncomeEditor.vue"
+import UserMenu from "../views/UserMenu.vue"
+import UserProfile from "../views/UserProfile.vue"
+import VerifyYourEmail from "../views/VerifyYourEmail.vue"
 
 const routes = [
   {
     path: '/',
     component: DefaultLayout,
     redirect: '/home',
-    meta: { requiresAuth: true },
+    meta: { requiresAuth: true, requiresVerifiedEmail: true },
     children: [
-      { path: '/home', name: 'Home', component: Home }
+      { path: '/home', name: 'Home', component: Home },
+      { path: '/monthly-bills', name: 'MonthlyBills', component: MonthlyBills },
+      { path: '/monthly-incomes', name: 'MonthlyIncomes', component: MonthlyIncomes },
+      { path: '/category/create', name: 'CategoryCreate', component: Category },
+      { path: '/category/:id', name: 'CategoryView', component: Category },
+      { path: '/expense/create', name: 'ExpenseCreator', component: ExpenseEditor },
+      { path: '/expense/:id', name: 'ExpenseEditor', component: ExpenseEditor },
+      { path: '/income/create', name: 'IncomeCreator', component: IncomeEditor },
+      { path: '/income/:id', name: 'IncomeEditor', component: IncomeEditor },
+      { path: '/user', name: 'UserProfile', component: UserProfile },
     ]
+  },
+  { 
+    path: '/verify-your-email',
+    name: 'VerifyYourEmail',
+    component: VerifyYourEmail,
+    meta: { requiresAuth: true, requiresVerifiedEmail: false },
+  },
+
+  {
+    path: '/user-menu',
+    name: 'UserMenu',
+    component: UserMenu,
+    meta: { requiresAuth: true, requiresVerifiedEmail: true },
   },
   {
     path: '/register',
@@ -35,10 +65,19 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  if (to.meta.requiresAuth && !store.state.user.token) {
+  const verifiedEmail = store.state.auth.user.verifiedEmail === 'true'
+  const hasSession = store.state.auth.user.token
+
+  if (to.meta.requiresAuth && !hasSession) {
     next({name: 'Login'})
   }
-  else if (store.state.user.token && to.meta.isGuest) {
+  else if (to.meta.requiresAuth && to.meta.requiresVerifiedEmail && !verifiedEmail) {
+    next({name: 'VerifyYourEmail'})
+  }
+  else if (to.meta.requiresAuth && !to.meta.requiresVerifiedEmail && verifiedEmail) {
+    next({name: 'Home'})
+  }
+  else if (to.meta.isGuest && hasSession && verifiedEmail) {
     next({name: 'Home'})
   }
   else {
