dd86053f1aa7e0c1eacaae0cb4982ebaee71cdef
Luciano Oliva <luciano@seg.eti.br>
2024-04-04 20:08:22 -0300

    Define some default collors

--- vue/tailwind.config.js
+++ vue/tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bodybg: '#f7f3e8',
+        'bodybg-darker': '#d7d3c6',
+        menubg: '#f7f7f7',
+        buttonbg: '#f7f7f7',
+        primary: '#e3686d',
+        'primary-darker': '#c3585d',
+        'primary-lighter': '#fb747a',
+        'good-green': '#41b883',
+        'good-green-darker': '#329167',
+      },
+    },
   },
   plugins: [],
 }
