e5ca6e915ac732582a3f647709daf4ff7d0ed4d3
Luciano Oliva <luciano@seg.eti.br>
2024-04-04 20:10:20 -0300

    Create Pie Chart reusable component

---
+++ vue/src/components/charts/PieChart.vue
@@ -0,0 +1,12 @@
+<template>
+  <Pie :data="props.data" :options="props.options" />
+</template>
+
+<script setup>
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Pie } from 'vue-chartjs'
+
+const props = defineProps(['data', 'options'])
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+</script>
