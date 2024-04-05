ae64b811b38afec462c73423f2b7df1b2a54199d
Luciano Oliva <luciano@seg.eti.br>
2024-04-04 22:29:19 -0300

    Fix vue/vite.config.js

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { viteServerHost, viteServerPort } from "../config/index.js"
+import { viteServerHost, viteServerPort } from "./config/index.js"
 
 
 // https://vitejs.dev/config/
